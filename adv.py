0d5c880f13bf15901775cb277c8df52c525ace27
Aaron Pleitez <pleitez.aaron12@gmail.com>
2020-05-15 12:56:33 -0500

    test passes test line.txt

--- adv.py
+++ adv.py
@@ -2,6 +2,9 @@ from room import Room
 from player import Player
 from world import World
 
+from util import Stack, Queue
+from graph import Graph
+
 import random
 from ast import literal_eval
 
@@ -14,10 +17,10 @@ world = World()
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -25,12 +28,70 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+# reverse helper
+
+
+def reverse(direction):
+    if direction == 'n':
+        return 's'
+    if direction == 's':
+        return 'n'
+    if direction == 'e':
+        return 'w'
+    if direction == 'w':
+        return 'e'
+
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
 traversal_path = []
+prev_room = None
+all_exits = None
+directions = None
+curr_room = player.current_room.id
+
+visited = {}
+
+s = Stack()
+s.push([directions, curr_room, prev_room])
 
 
+while s.size() > 0:
+    path = s.pop()
+    direction = path[0]
+    current_room = path[1]
+    prev_room = path[2]
 
+    print(current_room)
+
+    if current_room not in visited:
+
+        visited[current_room] = {}
+        exits = player.current_room.get_exits()
+
+        for room_exit in exits:
+            visited[current_room][room_exit] = '?'
+
+        print(visited, "this is visited")
+
+        for key, value in visited.items():
+
+            for index, item in value.items():
+
+                print(item, "item")
+                if item == '?':
+                    prev_room = current_room
+                    print(prev_room, "------------")
+                    player.travel(index)
+                    traversal_path.append(index)
+                    value[index] = player.current_room.id
+                    s.push([index,  player.current_room.id, prev_room])
+
+print(visited)
+
+
+# ALL ITEMS ARE IN THE
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -41,22 +102,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
 # UNCOMMENT TO WALK AROUND
 #######
 player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
